35cc80186a65791a4150ebe0f2b52ed7b09ac184
Mike Sgroi <msgroi@msgroi-ltmdbah.internal.salesforce.com>
2025-04-11 18:18:41 -0700

    add back reference to Calculator, update .gitignore

--- src/main/java/com/example/JavaMain.java
+++ src/main/java/com/example/JavaMain.java
@@ -1,10 +1,10 @@
 package com.example;
 
-//import com.example.Calculator;
+import com.example.Calculator;
 
 public class JavaMain {
     public static void main(String[] args) {
-        //Calculator calc = new Calculator();
-        //System.out.println("5 + 3 = " + calc.add(5, 3));
+        Calculator calc = new Calculator();
+        System.out.println("5 + 3 = " + calc.add(5, 3));
     }
 } 
